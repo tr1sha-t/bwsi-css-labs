8a2e0588a48f38e40228cb6b8a3762f30ed35bc3
tr1sha-t <trisha.thyagarajan@gmail.com>
2026-03-30 20:13:55 -0700

    Added intro

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Trisha_1 Thyagarajan" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi, my name is Trisha_1 Thyagarajan and I’m excited to learn coding in this course!")
 
 if __name__ == "__main__":
     main()
